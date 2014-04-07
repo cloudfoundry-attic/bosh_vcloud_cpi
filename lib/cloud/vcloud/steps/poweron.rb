f2e28978f0f800ea381efb944d3d97d64300abbf
Justin Yu <justiny@vmware.com>
2014-04-07 11:14:29 -0700

    Modify the rollback methods to clean up VMs when getting the different create_vm failure.
    Change-Id: I7819283ce53a5967df835cdb7bf40dd7b4c92c9a

--- lib/cloud/vcloud/steps/poweron.rb
+++ lib/cloud/vcloud/steps/poweron.rb
@@ -9,8 +9,27 @@ module VCloudCloud
         end
         poweron_link = entity.power_on_link
         raise "#{entity.name} unable to power on" unless poweron_link
+        state[:poweron_ref] = ref
         client.invoke_and_wait :post, poweron_link
         state[ref] = client.reload entity
+      end
+
+      def rollback
+        ref = state[:poweron_ref]
+        if ref
+          begin
+            entity = client.reload ref
+            if entity['status'] == VCloudSdk::Xml::RESOURCE_ENTITY_STATUS[:POWERED_OFF].to_s
+              @logger.debug "#{entity.name} already powered off"
+              return
+            end
+
+            poweroff_link = entity.power_off_link
+            client.invoke_and_wait :post, poweroff_link
+          rescue => ex
+            @logger.debug(ex) if @logger
+          end
+        end
       end
     end
   end
